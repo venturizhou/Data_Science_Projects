e78406be252750e7b41949e517b5c1a2afa8d2be
Steven <venturizhou@gmail.com>
2020-09-05 01:32:08 -0400

    testing selenium

--- Used Phones/tester.py
+++ Used Phones/tester.py
@@ -1,8 +1,27 @@
 from selenium import webdriver
 
-PATH = "/usr/local/bin/chromedriver"
-driver = webdriver.Chrome(PATH)
+PATH = "C:/Users/venturi/Work/Data_Science_Projects/Used Phones/msedgedriver.exe"
+driver = webdriver.Edge(PATH)
 
-driver.get("https://www.google.com")
-print(driver.title)
-driver.quit()
+driver.get("https://swappa.com")
+
+def scrape_iPhone_11():
+    # driver.find_element_by_css_selector("[title='Cheap iPhone 11'").click()
+    ##unlocked iPhones
+    driver.find_element_by_css_selector("[title='Cheap iPhone 11']").click()
+    driver.find_element_by_css_selector("[title*='unlocked']").click()
+    temp = driver.find_element_by_id('section_more').find_elements_by_partial_link_text("listing")
+    print(temp)
+    # driver.find_element_by_css_selector("[title*='AT&T']").click()
+    # driver.find_element_by_css_selector("[title*='Sprint']").click()
+    # driver.find_element_by_css_selector("[title*='T-Mobile']").click()
+    # driver.find_element_by_css_selector("[title*='Verizon']").click()
+
+driver.find_element_by_css_selector("[title=iPhones").click()
+driver.find_element_by_css_selector("[title='Cheap iPhone 11']").click()
+# driver.find_element_by_css_selector("[title*='Unlocked']").click()
+link = driver.find_element_by_css_selector("a[href*='unlocked']")
+print(link.get_attribute('innerHTML'))
+# scrape_iPhone_11()
+# driver.quit()
+
